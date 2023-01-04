ebd06217074c10810b37981cf2527bcd3cc56c08
leehyeryeong <w2108@e-mirim.hs.kr>
2023-01-04 14:46:08 +0900

    몸무게가 큰 사람의 수를 구하는 함수 수정하기

---
+++ CosProC_2022winter/weightTest.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//몸무게가 큰 사람의 수를 구하는 함수 수정하기
+int solution(int weight, int boxes[], int boxes_len) {
+	int answer = 0;
+
+	for (int i = 0; i < boxes_len; i++) {
+		if () {
+			answer++;
+		}
+	}
+	
+	return answer;
+}
+int main() {
+	int weight = 600;
+	int boxes[5] = { 653, 670, 533, 540, 660 };
+	int boxes_len = 5;
+	
+	
+	return 0;
+}
