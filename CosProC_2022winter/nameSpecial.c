1fe9381775c288a4f6cf00d93e3b61440e3804e6
leehyeryeong <w2108@e-mirim.hs.kr>
2023-01-04 15:16:56 +0900

    이름에 특정 문자가 포함된 개수를 구하는 함수 수정하기

---
+++ CosProC_2022winter/nameSpecial.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//이름에 특정 문자가 포함된 개수를 구하는 함수 수정하기
+int solution(char* name_list, int name_list_len) {
+	int answer = 0;
+	for (int i = 0; i < name_list_len; ++i) {
+		for (int j = 0; name_list[i][j] < name_list_len; ++j) {
+			if (name_list[i][j] == 'j' || name_list[i][j] == 'k') {
+				answer++;
+				break;
+			}
+		}
+	}
+	return answer;
+}
+int main() {
+
+	return 0;
+}
